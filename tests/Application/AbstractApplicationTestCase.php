0eb7ab1b94de36d0dfe76701ef35d89deb749f69
Jon Cram <webignition@gmail.com>
2023-09-22 17:35:34 +0100

    Create application-level tests (#15)

---
+++ tests/Application/AbstractApplicationTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application;
+
+use App\Tests\Services\ApplicationClient\Client;
+use App\Tests\Services\ApplicationClient\ClientFactory;
+use SmartAssert\SymfonyTestClient\ClientInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+abstract class AbstractApplicationTestCase extends WebTestCase
+{
+    protected static KernelBrowser $kernelBrowser;
+    protected static Client $staticApplicationClient;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        self::$kernelBrowser = self::createClient();
+
+        $factory = self::getContainer()->get(ClientFactory::class);
+        \assert($factory instanceof ClientFactory);
+
+        self::$staticApplicationClient = $factory->create(static::getClientAdapter());
+    }
+
+    public static function getClientAdapter(): ClientInterface
+    {
+        return \Mockery::mock(ClientInterface::class);
+    }
+}
---
+++ tests/Services/ApplicationClient/Client.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Services\ApplicationClient;
+
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\SymfonyTestClient\ClientInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+readonly class Client
+{
+    public function __construct(
+        private ClientInterface $client,
+        private RouterInterface $router,
+    ) {
+    }
+
+    public function makeSignInPageReadRequest(?string $userIdentifier, string $method = 'GET'): ResponseInterface
+    {
+        $queryParameters = [];
+        if (null !== $userIdentifier) {
+            $queryParameters['user-identifier'] = $userIdentifier;
+        }
+
+        return $this->client->makeRequest($method, $this->router->generate('sign-in', $queryParameters));
+    }
+
+    public function makeSignInPageWriteRequest(
+        ?string $userIdentifier,
+        ?string $password,
+        string $method = 'POST'
+    ): ResponseInterface {
+        $payload = [];
+
+        if (is_string($userIdentifier)) {
+            $payload['user-identifier'] = $userIdentifier;
+        }
+
+        if (is_string($password)) {
+            $payload['password'] = $password;
+        }
+
+        return $this->client->makeRequest(
+            $method,
+            $this->router->generate('sign-in'),
+            ['Content-Type' => 'application/x-www-form-urlencoded'],
+            http_build_query($payload)
+        );
+    }
+}
