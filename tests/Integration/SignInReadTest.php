ab1a49bdebbb967e4a39c02c2073d89927c8067f
Jon Cram <webignition@gmail.com>
2023-09-22 18:13:27 +0100

    Dockerise (#16)

---
+++ tests/Integration/SignInReadTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+use App\Tests\Application\AbstractSignInReadTest;
+
+class SignInReadTest extends AbstractSignInReadTest
+{
+    use GetClientAdapterTrait;
+}
