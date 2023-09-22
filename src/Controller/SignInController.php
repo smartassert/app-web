f49c7fb96ced8e0debe65b68ba022ae73111e811
Jon Cram <webignition@gmail.com>
2023-09-22 12:30:46 +0100

    Create minimal login page

---
+++ src/Controller/SignInController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment as TwigEnvironment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class SignInController
+{
+    public function __construct(
+        private readonly TwigEnvironment $twig,
+        private readonly RouterInterface $router,
+    ) {
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    #[Route('/sign-in/', name: 'sign-in', methods: ['GET','POST'])]
+    public function index(Request $request): Response
+    {
+        if (Request::METHOD_POST === $request->getMethod()) {
+            return new RedirectResponse($this->router->generate('sign-in'));
+        }
+
+        return new Response($this->twig->render('sign_in/index.html.twig'));
+    }
+}
