11642d6c7eed5fd36653a7d23c4bc632868f21bd
Jon Cram <webignition@gmail.com>
2023-09-28 11:10:41 +0100

    Render email address in sign-in form

--- tests/Functional/Application/SignInReadTest.php
+++ tests/Functional/Application/SignInReadTest.php
@@ -5,8 +5,23 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Application;
 
 use App\Tests\Application\AbstractSignInReadTest;
+use Symfony\Component\Routing\RouterInterface;
 
 class SignInReadTest extends AbstractSignInReadTest
 {
     use GetClientAdapterTrait;
+
+    public function testReadRendersUserIdentifier(): void
+    {
+        $router = self::getContainer()->get(RouterInterface::class);
+        \assert($router instanceof RouterInterface);
+
+        $email = md5((string) rand()) . '@example.com';
+
+        $crawler = self::$kernelBrowser->request('GET', $router->generate('sign_in_view', ['email' => $email]));
+
+        $renderedEmail = $crawler->filter('input[name=user-identifier]')->attr('value');
+
+        self::assertSame($email, $renderedEmail);
+    }
 }
