4d5ed3d6816f9919fc2d2bc35109b3ecfed90429
Jon Cram <webignition@gmail.com>
2023-09-29 15:58:52 +0100

    Use token in cookie to authenticate

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -4,15 +4,31 @@ declare(strict_types=1);
 
 namespace App\Security;
 
+use App\RedirectRoute\Factory;
+use App\RedirectRoute\Serializer;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 
-class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
+readonly class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
 {
+    public function __construct(
+        private Factory $redirectRouteFactory,
+        private UrlGeneratorInterface $urlGenerator,
+        private Serializer $redirectRouteSerializer,
+    ) {
+    }
+
     public function start(Request $request, AuthenticationException $authException = null): Response
     {
-        return new Response(null, Response::HTTP_UNAUTHORIZED);
+        $redirectRoute = $this->redirectRouteFactory->createFromRequest($request);
+
+        return new RedirectResponse($this->urlGenerator->generate(
+            'sign_in_view',
+            ['route' => $this->redirectRouteSerializer->serialize($redirectRoute)]
+        ));
     }
 }
