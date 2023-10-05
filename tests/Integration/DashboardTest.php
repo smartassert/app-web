c82d2ba99b252631c9fa759312bef7fd9a04cfcf
Jon Cram <webignition@gmail.com>
2023-10-05 17:21:38 +0100

    Add test

--- tests/Integration/DashboardTest.php
+++ tests/Integration/DashboardTest.php
@@ -5,8 +5,45 @@ declare(strict_types=1);
 namespace App\Tests\Integration;
 
 use App\Tests\Application\AbstractDashboardTest;
+use SmartAssert\ApiClient\Model\RefreshableToken;
+use SmartAssert\TestAuthenticationProviderBundle\FrontendTokenProvider;
 
 class DashboardTest extends AbstractDashboardTest
 {
     use GetClientAdapterTrait;
+
+    public function testExpiredUserTokenIsRefreshed(): void
+    {
+        $frontendTokenProvider = self::getContainer()->get(FrontendTokenProvider::class);
+        \assert($frontendTokenProvider instanceof FrontendTokenProvider);
+
+        $frontendToken = $frontendTokenProvider->get('user@example.com');
+
+        $response = self::$staticApplicationClient->makeDashboardReadRequest(new RefreshableToken(
+            $frontendToken->token,
+            $frontendToken->refreshToken
+        ));
+
+        self::assertSame(200, $response->getStatusCode());
+
+        $jwtTokenTtl = $this->getUsersServiceJwtTokenTtl();
+        $waitTime = $jwtTokenTtl + 1;
+
+        sleep($waitTime);
+
+        $response = self::$staticApplicationClient->makeDashboardReadRequest(new RefreshableToken(
+            $frontendToken->token,
+            $frontendToken->refreshToken
+        ));
+
+        self::assertSame(200, $response->getStatusCode());
+    }
+
+    private function getUsersServiceJwtTokenTtl(): int
+    {
+        $usersServicePrintEnvOutput =
+            `docker-compose -f tests/build/docker-compose.yml exec users-service printenv | grep JWT_TOKEN_TTL`;
+
+        return (int) str_replace('JWT_TOKEN_TTL=', '', $usersServicePrintEnvOutput);
+    }
 }
