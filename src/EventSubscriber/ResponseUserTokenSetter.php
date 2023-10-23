360d6541cec168464d230b7beda3a8e6d1cb3ec8
Jon Cram <webignition@gmail.com>
2023-10-23 17:17:27 +0100

    Invalidate token cookie on success

--- src/EventSubscriber/ResponseUserTokenSetter.php
+++ src/EventSubscriber/ResponseUserTokenSetter.php
@@ -6,17 +6,21 @@ namespace App\EventSubscriber;
 
 use App\RefreshableToken\Encrypter;
 use App\Security\User;
+use App\SignInRedirectResponse\Factory;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 readonly class ResponseUserTokenSetter implements EventSubscriberInterface
 {
     public function __construct(
         private Security $security,
         private Encrypter $tokenEncrypter,
+        private Factory $signInRedirectResponseFactory,
     ) {
     }
 
@@ -27,12 +31,15 @@ readonly class ResponseUserTokenSetter implements EventSubscriberInterface
     {
         return [
             KernelEvents::RESPONSE => [
-                ['setUserTokenInResponse', 0],
+                ['set', 0],
+            ],
+            LogoutEvent::class => [
+                ['remove', 0],
             ],
         ];
     }
 
-    public function setUserTokenInResponse(ResponseEvent $event): void
+    public function set(ResponseEvent $event): void
     {
         $response = $event->getResponse();
         $user = $this->security->getUser();
@@ -43,6 +50,14 @@ readonly class ResponseUserTokenSetter implements EventSubscriberInterface
                 $this->tokenEncrypter->encrypt($user->getSecurityToken())
             ));
         }
+
+        $event->setResponse($response);
+    }
+
+    public function remove(LogoutEvent $event): void
+    {
+        $response = $this->signInRedirectResponseFactory->create(userIdentifier: null, route: null);
+        $response->headers->setCookie(Cookie::create('token'));
 
         $event->setResponse($response);
     }
