9d01d691bfb2654f098e2c95c1981244f9408eba
Jon Cram <webignition@gmail.com>
2023-11-24 10:15:21 +0000

    Fix CS

--- src/EventSubscriber/UserTokenRevoker.php
+++ src/EventSubscriber/UserTokenRevoker.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
-use App\RefreshableToken\Encrypter;
-use App\Response\RedirectResponseFactory;
 use App\Security\User;
 use SmartAssert\ApiClient\UsersClient;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 readonly class UserTokenRevoker implements EventSubscriberInterface
@@ -20,8 +15,6 @@ readonly class UserTokenRevoker implements EventSubscriberInterface
     public function __construct(
         private Security $security,
         private UsersClient $usersClient,
-        private Encrypter $tokenEncrypter,
-        private RedirectResponseFactory $redirectResponseFactory,
     ) {
     }
 
