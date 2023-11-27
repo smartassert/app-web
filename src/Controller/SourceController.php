25e049233e479d95d736971ea43ddb15faa14e5f
Jon Cram <webignition@gmail.com>
2023-11-27 16:48:28 +0000

    Render separate sources page (#176)

---
+++ src/Controller/SourceController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Security\ApiKey;
+use SmartAssert\ApiClient\SourceClient;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment as TwigEnvironment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+readonly class SourceController
+{
+    public function __construct(
+        private TwigEnvironment $twig,
+        private SourceClient $sourceClient,
+    ) {
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
+    #[Route('/sources', name: 'sources', methods: ['GET'])]
+    public function index(ApiKey $apiKey): Response
+    {
+        return new Response($this->twig->render('source/index.html.twig', [
+            'sources' => $this->sourceClient->list($apiKey->key),
+        ]));
+    }
+}
