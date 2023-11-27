d32b7c2491e9ab3f4430e37fc64340010040c52f
Jon Cram <webignition@gmail.com>
2023-11-27 17:01:39 +0000

    Render add file source form (#177)

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Response\RedirectResponse;
 use App\Security\ApiKey;
 use SmartAssert\ApiClient\SourceClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Environment as TwigEnvironment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
@@ -18,6 +20,7 @@ readonly class SourceController
     public function __construct(
         private TwigEnvironment $twig,
         private SourceClient $sourceClient,
+        private UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
@@ -32,5 +35,11 @@ readonly class SourceController
         return new Response($this->twig->render('source/index.html.twig', [
             'sources' => $this->sourceClient->list($apiKey->key),
         ]));
+    }
+
+    #[Route('/sources/file', name: 'sources_add_file_source', methods: ['POST'])]
+    public function addFileSource(): Response
+    {
+        return new RedirectResponse($this->urlGenerator->generate('sources'));
     }
 }
