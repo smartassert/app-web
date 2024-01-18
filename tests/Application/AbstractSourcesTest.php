93feb394f513d6244232dc88741974feed24a4eb
Jon Cram <webignition@gmail.com>
2024-01-18 16:04:06 +0000

    Handle file source creation submission (#192)

---
+++ tests/Application/AbstractSourcesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application;
+
+use App\Enum\Routes;
+use App\RedirectRoute\RedirectRoute;
+use App\RedirectRoute\Serializer;
+use App\Tests\Services\RequestCookieFactory;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+abstract class AbstractSourcesTest extends AbstractApplicationTestCase
+{
+    public function testGetInvalidToken(): void
+    {
+        self::$kernelBrowser->getCookieJar()->clear();
+
+        $response = self::$staticApplicationClient->makeSourcesReadRequest('token=invalid');
+        self::assertSame(302, $response->getStatusCode());
+
+        $urlGenerator = self::getContainer()->get(UrlGeneratorInterface::class);
+        \assert($urlGenerator instanceof UrlGeneratorInterface);
+
+        $redirectRouteSerializer = self::getContainer()->get(Serializer::class);
+        \assert($redirectRouteSerializer instanceof Serializer);
+
+        $expectedRedirectRoute = new RedirectRoute(Routes::SOURCES_NAME->value, []);
+        $expected = $urlGenerator->generate(
+            Routes::SIGN_IN_VIEW_NAME->value,
+            ['route' => $redirectRouteSerializer->serialize($expectedRedirectRoute)]
+        );
+
+        self::assertSame($expected, $response->getHeaderLine('location'));
+    }
+
+    public function testGetSuccess(): void
+    {
+        $requestCookieFactory = self::getContainer()->get(RequestCookieFactory::class);
+        \assert($requestCookieFactory instanceof RequestCookieFactory);
+
+        $requestCookie = $requestCookieFactory->create(self::$staticApplicationClient, $this->getSessionIdentifier());
+
+        $response = self::$staticApplicationClient->makeSourcesReadRequest($requestCookie);
+        self::assertSame(200, $response->getStatusCode());
+        self::assertStringContainsString('text/html', $response->getHeaderLine('content-type'));
+
+        $requestCookie = $requestCookieFactory->createFromResponse($response, $this->getSessionIdentifier());
+        $response = self::$staticApplicationClient->makeSourcesReadRequest($requestCookie);
+        self::assertSame(200, $response->getStatusCode());
+        self::assertStringContainsString('text/html', $response->getHeaderLine('content-type'));
+    }
+}
