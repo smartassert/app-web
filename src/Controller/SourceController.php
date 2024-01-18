ccfa5b273decf21333205a89942ab41f3152240a
Jon Cram <webignition@gmail.com>
2024-01-18 16:04:06 +0000

    Handle file source creation submission (#192)

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -4,9 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Enum\Routes;
 use App\Response\RedirectResponse;
 use App\Security\ApiKey;
+use SmartAssert\ApiClient\Exception\Error\ErrorException;
+use SmartAssert\ApiClient\Exception\Http\HttpClientException;
+use SmartAssert\ApiClient\Exception\Http\HttpException;
+use SmartAssert\ApiClient\Exception\Http\NotFoundException;
+use SmartAssert\ApiClient\Exception\Http\UnauthorizedException;
+use SmartAssert\ApiClient\Exception\Http\UnexpectedContentTypeException;
+use SmartAssert\ApiClient\Exception\Http\UnexpectedDataException;
+use SmartAssert\ApiClient\Exception\IncompleteDataException;
+use SmartAssert\ApiClient\FileSourceClient;
 use SmartAssert\ApiClient\SourceClient;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -20,6 +31,7 @@ readonly class SourceController
     public function __construct(
         private TwigEnvironment $twig,
         private SourceClient $sourceClient,
+        private FileSourceClient $fileSourceClient,
         private UrlGeneratorInterface $urlGenerator,
     ) {
     }
@@ -29,7 +41,7 @@ readonly class SourceController
      * @throws SyntaxError
      * @throws LoaderError
      */
-    #[Route('/sources', name: 'sources', methods: ['GET'])]
+    #[Route('/sources', name: Routes::SOURCES_NAME->value, methods: ['GET'])]
     public function index(ApiKey $apiKey): Response
     {
         return new Response($this->twig->render('source/index.html.twig', [
@@ -37,9 +49,21 @@ readonly class SourceController
         ]));
     }
 
+    /**
+     * @throws UnauthorizedException
+     * @throws ErrorException
+     * @throws IncompleteDataException
+     * @throws HttpException
+     * @throws NotFoundException
+     * @throws UnexpectedContentTypeException
+     * @throws HttpClientException
+     * @throws UnexpectedDataException
+     */
     #[Route('/sources/file', name: 'sources_add_file_source', methods: ['POST'])]
-    public function addFileSource(): Response
+    public function addFileSource(ApiKey $apiKey, Request $request): Response
     {
+        $this->fileSourceClient->create($apiKey->key, $request->request->getString('label'));
+
         return new RedirectResponse($this->urlGenerator->generate('sources'));
     }
 }
