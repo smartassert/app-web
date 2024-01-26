0eff6a028c82cc457bdf5cd57b5270e38bb6f6c0
Jon Cram <webignition@gmail.com>
2024-01-26 17:39:07 +0000

    Verify

--- tests/Functional/Application/SourcesTest.php
+++ tests/Functional/Application/SourcesTest.php
@@ -118,4 +118,85 @@ class SourcesTest extends AbstractSourcesTest
             $response->headers->get('location')
         );
     }
+
+    public function testAddFileSourceBadRequest(): void
+    {
+        $sourcesDataRepository = new DataRepository(
+            'pgsql:host=localhost;port=5432;dbname=sources;user=postgres;password=password!'
+        );
+        $sourcesDataRepository->removeAllFor(['file_source', 'git_source', 'source']);
+
+        $urlGenerator = self::getContainer()->get(UrlGeneratorInterface::class);
+        \assert($urlGenerator instanceof UrlGeneratorInterface);
+
+        $requestCookieFactory = self::getContainer()->get(RequestCookieFactory::class);
+        \assert($requestCookieFactory instanceof RequestCookieFactory);
+
+        $requestCookie = $requestCookieFactory->create($this->applicationClient, $this->getSessionIdentifier());
+
+        $sourcesUrl = $urlGenerator->generate(Routes::SOURCES_NAME->value);
+
+        $crawler = $this->kernelBrowser->request(
+            method: 'GET',
+            uri: $sourcesUrl,
+            server: [
+                'HTTP_COOKIE' => $requestCookie,
+            ]
+        );
+
+        self::assertSame(200, $this->kernelBrowser->getResponse()->getStatusCode());
+
+        $sourcesList = $crawler->filter('#sources_list');
+        self::assertSame(0, $sourcesList->count());
+
+        $label = '';
+
+        $formElement = $crawler->filter('#file_source_add');
+        self::assertNull($formElement->attr('class'));
+
+        $errorContainer = $formElement->filter('span.error');
+        self::assertSame(0, $errorContainer->count());
+
+        $formFieldLabel = $formElement->filter('[for=file_source_add_label]');
+        self::assertNull($formFieldLabel->attr('class'));
+
+        $formField = $formElement->filter('#file_source_add_label');
+        self::assertNull($formField->attr('class'));
+
+        $addFileSourceForm = $formElement->filter('input[type=submit]')->form([
+            'label' => $label,
+        ]);
+
+        $this->kernelBrowser->submit($addFileSourceForm);
+
+        $response = $this->kernelBrowser->getResponse();
+        self::assertSame(302, $response->getStatusCode());
+        self::assertSame($sourcesUrl, $response->headers->get('location'));
+
+        $crawler = $this->kernelBrowser->request(
+            method: 'GET',
+            uri: $sourcesUrl,
+            server: [
+                'HTTP_COOKIE' => $requestCookie,
+            ]
+        );
+
+        self::assertSame(200, $this->kernelBrowser->getResponse()->getStatusCode());
+
+        $sourcesList = $crawler->filter('#sources_list');
+        self::assertSame(0, $sourcesList->count());
+
+        $formElement = $crawler->filter('#file_source_add');
+        self::assertSame('error', $formElement->attr('class'));
+
+        $errorContainer = $formElement->filter('span.error');
+        self::assertSame(1, $errorContainer->count());
+        self::assertSame('This value must not be empty.', $errorContainer->innerText());
+
+        $formFieldLabel = $formElement->filter('[for=file_source_add_label]');
+        self::assertSame('error', $formFieldLabel->attr('class'));
+
+        $formField = $formElement->filter('#file_source_add_label');
+        self::assertSame('error', $formField->attr('class'));
+    }
 }
