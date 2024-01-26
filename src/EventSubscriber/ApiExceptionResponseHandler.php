3466f8786b5e0c5cbc813a1d2b47f40dd70320fb
Jon Cram <webignition@gmail.com>
2024-01-26 09:27:54 +0000

    Handle api ErrorException (#223)

--- src/EventSubscriber/ApiExceptionResponseHandler.php
+++ src/EventSubscriber/ApiExceptionResponseHandler.php
@@ -4,18 +4,19 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
-use App\Enum\SignInErrorState;
 use App\Exception\ApiException;
 use App\RedirectRoute\Factory;
+use App\RedirectRoute\RedirectRoute;
 use App\Response\RedirectResponseFactory;
+use SmartAssert\ApiClient\Exception\ErrorExceptionInterface;
 use SmartAssert\ApiClient\Exception\Http\UnauthorizedException;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
-readonly class ApiExceptionHandler implements EventSubscriberInterface
+readonly class ApiExceptionResponseHandler implements EventSubscriberInterface
 {
     public function __construct(
         private Security $security,
@@ -43,28 +44,39 @@ readonly class ApiExceptionHandler implements EventSubscriberInterface
             return;
         }
 
+        $response = null;
+
         $exception = $throwable->exception;
-        if (!$exception instanceof UnauthorizedException) {
-            return;
+        if ($exception instanceof ErrorExceptionInterface) {
+            $response = $this->handleErrorException($throwable->redirectRoute);
         }
 
-        $session = $event->getRequest()->getSession();
-        if (!$session instanceof Session) {
-            return;
+        if ($exception instanceof UnauthorizedException) {
+            $response = $this->handleUnauthorizedException();
         }
 
+        if ($response instanceof Response) {
+            $event->setResponse($response);
+        }
+    }
+
+    private function handleErrorException(?RedirectRoute $redirectRoute): ?Response
+    {
+        return $redirectRoute instanceof RedirectRoute
+            ? $this->redirectResponseFactory->create($redirectRoute)
+            : null;
+    }
+
+    private function handleUnauthorizedException(): ?Response
+    {
         $user = $this->security->getUser();
         if (null === $user) {
-            return;
+            return null;
         }
 
-        $session->getFlashBag()->set('error', SignInErrorState::API_UNAUTHORIZED->value);
-
-        $response = $this->redirectResponseFactory->createForSignIn(
+        return $this->redirectResponseFactory->createForSignIn(
             $user->getUserIdentifier(),
             $this->redirectRouteFactory->getDefault(),
         );
-
-        $event->setResponse($response);
     }
 }
--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Exception;
 
 use App\Enum\ApiService;
+use App\RedirectRoute\RedirectRoute;
 
 class ApiException extends \Exception
 {
     public function __construct(
         public readonly ApiService $apiService,
-        public readonly \Throwable $exception
+        public readonly \Throwable $exception,
+        public readonly ?RedirectRoute $redirectRoute = null,
     ) {
         parent::__construct($exception->getMessage(), $exception->getCode(), $exception->getPrevious());
     }
