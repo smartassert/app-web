beb5bb019a6e64e070e7b1a392e48ee3a0cfbed8
Jon Cram <webignition@gmail.com>
2024-01-31 12:07:28 +0000

    Verify kernel.exception with ApiException error exception sets error details in session (#233)

---
+++ tests/Functional/EventHanding/KernelExceptionEvent/ApiException/ErrorExceptionTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\EventHanding\KernelExceptionEvent\ApiException;
+
+use App\Enum\ApiService;
+use App\Exception\ApiException;
+use App\RedirectRoute\RedirectRoute;
+use App\Tests\Services\SessionHandler;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ApiClient\Exception\Error\ErrorException;
+use SmartAssert\ServiceRequest\Error\ErrorInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class ErrorExceptionTest extends WebTestCase
+{
+    public function testErrorNameAndErrorAreSetInSession(): void
+    {
+        $client = self::createClient();
+
+        $sessionHandler = self::getContainer()->get(SessionHandler::class);
+        \assert($sessionHandler instanceof SessionHandler);
+
+        $session = $sessionHandler->create();
+        $sessionHandler->persist($client, $session);
+
+        $eventDispatcher = self::getContainer()->get(EventDispatcherInterface::class);
+        \assert($eventDispatcher instanceof EventDispatcherInterface);
+        \assert($eventDispatcher instanceof EventDispatcher);
+
+        $kernel = self::getContainer()->get(KernelInterface::class);
+        \assert($kernel instanceof KernelInterface);
+
+        $request = \Mockery::mock(Request::class);
+        $request
+            ->shouldReceive('getSession')
+            ->andReturn($session)
+        ;
+
+        $response = \Mockery::mock(ResponseInterface::class);
+        $response
+            ->shouldReceive('getStatusCode')
+            ->andReturn(400)
+        ;
+
+        $errorExceptionName = md5((string) rand());
+        $error = \Mockery::mock(ErrorInterface::class);
+
+        $errorException = new ErrorException(
+            $errorExceptionName,
+            \Mockery::mock(RequestInterface::class),
+            $response,
+            $error,
+        );
+
+        $exception = new ApiException(
+            ApiService::SOURCES,
+            $errorException,
+            new RedirectRoute('dashboard')
+        );
+
+        $event = new ExceptionEvent($kernel, $request, HttpKernelInterface::MAIN_REQUEST, $exception);
+        $eventDispatcher->dispatch($event, 'kernel.exception');
+
+        self::assertSame($errorExceptionName, $session->getFlashBag()->get('error_name')[0]);
+        self::assertSame($error, $session->get('error'));
+    }
+}
