da1f33a66a2dd047f66773bd3f276527a515c5b4
Jon Cram <webignition@gmail.com>
2024-02-13 12:46:30 +0000

    populate model

--- src/Response/Target.php
+++ src/Response/Target.php
@@ -6,5 +6,41 @@ namespace App\Response;
 
 readonly class Target
 {
+    /**
+     * @var non-empty-string[]
+     */
+    private array $sources;
 
+    /**
+     * @param non-empty-string   $name
+     * @param non-empty-string[] $sources
+     */
+    public function __construct(
+        private string $name,
+        array $sources,
+    ) {
+        $filteredSources = [];
+
+        foreach ($sources as $source) {
+            $source = trim($source);
+
+            if ('' !== $source) {
+                $filteredSources[] = $source;
+            }
+        }
+
+        $this->sources = $filteredSources;
+    }
+
+    /**
+     * @return ?non-empty-string
+     */
+    public function getName(string $source): ?string
+    {
+        if (in_array($source, $this->sources)) {
+            return $this->name;
+        }
+
+        return null;
+    }
 }
