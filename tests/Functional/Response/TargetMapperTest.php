c7e474044d6474169869c0903a50b94b0adb5693
Jon Cram <webignition@gmail.com>
2024-02-14 09:41:42 +0000

    Test TargetMapper (#264)

---
+++ tests/Functional/Response/TargetMapperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Response;
+
+use App\Response\TargetMapper;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+
+class TargetMapperTest extends WebTestCase
+{
+    /**
+     * @dataProvider targetMapperConfigurationDataProvider
+     */
+    public function testTargetMapperConfiguration(Request $request, ?string $expected): void
+    {
+        $targetMapper = self::getContainer()->get(TargetMapper::class);
+        \assert($targetMapper instanceof TargetMapper);
+
+        self::assertSame($expected, $targetMapper->getForRequest($request));
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function targetMapperConfigurationDataProvider(): array
+    {
+        return [
+            'request has no _route attribute' => [
+                'request' => (function () {
+                    $request = \Mockery::mock(Request::class);
+                    $request->attributes = new ParameterBag();
+
+                    return $request;
+                })(),
+                'expected' => null,
+            ],
+            'sign in => dashboard' => [
+                'request' => (function () {
+                    $request = \Mockery::mock(Request::class);
+                    $request->attributes = new ParameterBag(['_route' => 'sign_in_view']);
+
+                    return $request;
+                })(),
+                'expected' => 'dashboard',
+            ],
+        ];
+    }
+}
